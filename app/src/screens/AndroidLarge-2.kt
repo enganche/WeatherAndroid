d43cbc87a47b5dff208caaf23984d841cef48c43
Anhtuanzzz <62417928+Anhtuanzzz@users.noreply.github.com>
2024-04-01 07:29:23 +0700

    Init

---
+++ app/src/screens/AndroidLarge-2.kt
@@ -0,0 +1,20 @@
+package com.your.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+
+class AndroidLarge-2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_android_large-_2)
+        
+        
+        
+        
+    }
+}
+    
